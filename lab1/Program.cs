31387fbb11c09aa589ca492c626fa0a83c67caf8
ArionStudio <adrian.rybaczuk.praca@gmail.com>
2024-10-08 13:33:30 +0200

    remove unused files

--- lab1/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/about", () => "About page...");
-app.MapGet("/contact", () => "Contact page...");
-app.MapGet("/product/{id}", (int id) => $"Product ID: {id}");
-
-app.Run();
